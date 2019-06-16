33be1640a713ab5013c168bfdc62cabe6077c968
sangeethshehan1998 <sangeethshehan1998@gmail.com>
2019-06-16 22:03:02 +0530

    added

--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {CustomerDto} from '../dto/customer-dto';
+import {ItemDto} from '../dto/item-dto';
 
 
 @Injectable({
@@ -12,4 +14,19 @@ export class ItemService {
   ) { }
 
 
+  addItem(itemDto) {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'Application/json');
+    headers = headers.append('operation', 'add');
+    console.log(itemDto);
+    console.log(`headers ${headers.keys()}`);
+    return this.http.post('http://localhost:8080/pos/item', itemDto, {headers});
+  }
+
+  getAllItems() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'Application/json');
+    headers = headers.append('operation', 'getAll');
+    return this.http.get<Array<ItemDto>>('http://localhost:8080/pos/item', {headers});
+  }
 }
