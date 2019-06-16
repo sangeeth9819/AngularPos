8950750561d457a039a7847e137bd0a73b33cfe2
sangeethshehan1998 <sangeethshehan1998@gmail.com>
2019-06-16 21:22:25 +0530

    added

--- src/app/dto/item-dto.ts
+++ src/app/dto/item-dto.ts
@@ -5,4 +5,5 @@ export class ItemDto {
   price: string;
   qty: string;
 
+
 }
--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,31 +1,15 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {CustomerDto} from '../dto/customer-dto';
-import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class CustomerService {
+export class ItemService {
 
   constructor(
     private http: HttpClient,
-  ) {
-  }
+  ) { }
 
-  addCustomer(customerDto): Observable<any> {
-    let headers = new HttpHeaders();
-    headers = headers.append('Content-Type', 'Application/json');
-    headers = headers.append('operation', 'add');
-    console.log(customerDto);
-    console.log(`headers ${headers.keys()}`);
-    return this.http.post('http://localhost:8080/pos/customer', customerDto, {headers});
-  }
 
-  getAllCustomers(): Observable<Array<CustomerDto>> {
-    let headers = new HttpHeaders();
-    headers = headers.append('Content-Type', 'Application/json');
-    headers = headers.append('operation', 'getAll');
-    return this.http.get<Array<CustomerDto>>('http://localhost:8080/pos/customer', {headers});
-  }
 }
