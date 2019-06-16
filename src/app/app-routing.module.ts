ea9528a5a31190e614441d179046da2cd964b07f
sangeethshehan1998 <sangeethshehan1998@gmail.com>
2019-06-16 20:24:17 +0530

    add json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, Route} from '@angular/router';
+import {MainComponent} from './view/main/main.component';
+import {DashboardComponent} from './view/dashboard/dashboard.component';
+import {CutomerComponent} from './view/cutomer/cutomer.component';
+import {ItemComponent} from './view/item/item.component';
+import {OrderComponent} from './view/order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'customer',
+        component: CutomerComponent
+      },
+      {
+        path: 'item',
+        component: ItemComponent
+      },
+      {
+        path: 'orders',
+        component: OrderComponent
+      },
+    ]
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/main/dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './view/particale/navbar/navbar.component';
+import { FooterComponent } from './view/particale/footer/footer.component';
+import { SidebarComponent } from './view/particale/sidebar/sidebar.component';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
+import { SlideShowComponent } from './slide-show/slide-show.component';
+import { WelcomeNoteComponent } from './welcome-note/welcome-note.component';
+import { MainComponent } from './view/main/main.component';
+import { CutomerComponent } from './view/cutomer/cutomer.component';
+import { ItemComponent } from './view/item/item.component';
+import { OrderComponent } from './view/order/order.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardComponent,
+    SlideShowComponent,
+    WelcomeNoteComponent,
+    MainComponent,
+    CutomerComponent,
+    ItemComponent,
+    OrderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
