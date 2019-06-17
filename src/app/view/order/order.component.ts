321a09cd249d17bc986d5f0618be7f2191a5889a
sangeethshehan1998 <sangeethshehan1998@gmail.com>
2019-06-17 11:02:03 +0530

    added

--- src/app/view/order/order.component.ts
+++ src/app/view/order/order.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CustomerService} from '../../service/customer.service';
+import {CustomerDto} from '../../dto/customer-dto';
+import {ItemService} from '../../service/item.service';
+import {ItemDto} from '../../dto/item-dto';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  private customerList: Array<CustomerDto> = [];
+  private ItemList: Array<ItemDto> = [];
+
+  itemId: number;
+  itemName: string;
+
+  constructor(
+    private customerService: CustomerService,
+    private itemService: ItemService
+  ) {
+  }
 
   ngOnInit() {
+    this.getAll();
+    this.getAllItem();
   }
+
+  getAll() {
+    this.customerService.getAllCustomers().subscribe(result => {
+      this.customerList = result;
+      console.log('Customer List Combo :- ' + JSON.stringify(this.customerList));
+    });
+
+  }
+
+  getAllItem() {
+    this.itemService.getAllItems().subscribe(result => {
+      this.ItemList = result;
+      console.log('item List :- ' + JSON.stringify(this.ItemList));
+    });
+  }
+
+  setPrice11(name: number) {
+    this.ItemList.filter = this.itemName.trim().toLowerCase();
+
+  }
+
 
 }
